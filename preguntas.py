191730ac06558852c26f931f578d32f199337b0a
andresjaramillotamayo <anfjaramillota@unal.edu.co>
2023-11-24 07:24:56 -0500

    Septimo Laboratorio

--- preguntas.py
+++ preguntas.py
@@ -16,20 +16,19 @@ def pregunta_01():
     Carga y separación de los datos en `X` `y`
     """
     # Lea el archivo `concrete.csv` y asignelo al DataFrame `df`
-    df = ____  
+    df = pd.read_csv("concrete.csv", sep=',')
 
     # Asigne la columna `strength` a la variable `y`.
-    ____ = ____  
+    y = df['strength']
 
     # Asigne una copia del dataframe `df` a la variable `X`.
-    ____ = ____.____(____)  
+    x = df.copy()
 
     # Remueva la columna `strength` del DataFrame `X`.
-    ____.____(____)  
+    x.drop('strength', axis=1, inplace=True)
 
     # Retorne `X` y `y`
-    return x, y
-
+    return x, y 
 
 def pregunta_02():
     """
@@ -37,24 +36,24 @@ def pregunta_02():
     """
 
     # Importe train_test_split
-    from ____ import ____
+    from sklearn.model_selection import train_test_split
 
     # Cargue los datos de ejemplo y asigne los resultados a `X` y `y`.
     x, y = pregunta_01()
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 12453. Use el 75% de los patrones para entrenamiento.
-    (  
-        x_train,  
-        x_test,  
-        y_train,  
-        y_test,  
-    ) = ____(  
-        ____,  
-        ____,  
-        test_size=____,  
-        random_state=____,  
-    )  
+    (
+        x_train,
+        x_test,
+        y_train,
+        y_test,
+    ) = train_test_split(
+        x,
+        y,
+        test_size=0.25,
+        random_state=12453,
+    )
 
     # Retorne `X_train`, `X_test`, `y_train` y `y_test`
     return x_train, x_test, y_train, y_test
@@ -68,23 +67,23 @@ def pregunta_03():
     # Importe MLPRegressor
     # Importe MinMaxScaler
     # Importe Pipeline
-    from ____ import ____
+    from sklearn.neural_network import MLPRegressor
+    from sklearn.preprocessing import MinMaxScaler
+    from sklearn.pipeline import Pipeline
 
-    # Cree un pipeline que contenga un estimador MinMaxScaler y un estimador
-    # MLPRegressor
+    # Cree un pipeline que contenga un estimador MinMaxScaler y un estimador MLPRegressor
     pipeline = Pipeline(
         steps=[
             (
                 "minmaxscaler",
-                ____(___),  
+                MinMaxScaler(),  
             ),
             (
                 "mlpregressor",
-                ____(____),  
+                MLPRegressor(),  
             ),
         ],
     )
-
     # Retorne el pipeline
     return pipeline
 
@@ -97,24 +96,24 @@ def pregunta_04():
     # Importe GridSearchCV
     from sklearn.model_selection import GridSearchCV
 
-    # Cree una malla de búsqueda para el objecto GridSearchCV
-    # con los siguientes parámetros de búesqueda:
+    # Cree una malla de búsqueda para el objeto GridSearchCV
+    # con los siguientes parámetros de búsqueda:
     #   * De 1 a 8 neuronas en la capa oculta
     #   * Activación con la función `relu`.
     #   * Tasa de aprendizaje adaptativa
-    #   * Momentun con valores de 0.7, 0.8 y 0.9
-    #   * Tasa de aprendijzaje inicial de 0.01, 0.05, 0.1
+    #   * Momento con valores de 0.7, 0.8 y 0.9
+    #   * Tasa de aprendizaje inicial de 0.01, 0.05, 0.1
     #   * Un máximo de 5000 iteraciones
     #   * Use parada temprana
 
     param_grid = {
-        ___: ____,  
-        ___: ____,  
-        ___: ____,  
-        ___: ____,  
-        ___: ____,  
-        ___: ____,  
-        ___: ____,  
+        'mlpregressor__hidden_layer_sizes': range(1, 9),
+        'mlpregressor__activation': ['relu'],
+        'mlpregressor__learning_rate': ['adaptive'],
+        'mlpregressor__momentum': [0.7, 0.8, 0.9],
+        'mlpregressor__learning_rate_init': [0.01, 0.05, 0.1],
+        'mlpregressor__max_iter': [5000],
+        'mlpregressor__early_stopping': [True],
     }
 
     estimator = pregunta_03()
@@ -126,8 +125,8 @@ def pregunta_04():
     gridsearchcv = GridSearchCV(
         estimator=estimator,
         param_grid=param_grid,
-        ___ = ____  
-        ___ = ____  
+        cv=5,
+        scoring='r2',
     )
 
     return gridsearchcv
@@ -139,7 +138,7 @@ def pregunta_05():
     """
 
     # Importe mean_squared_error
-    from ____ import ____
+    from sklearn.metrics import mean_squared_error
 
     # Cargue las variables.
     x_train, x_test, y_train, y_test = pregunta_02()
@@ -148,21 +147,16 @@ def pregunta_05():
     estimator = pregunta_04()
 
     # Entrene el estimador
-    estimator.fit(x_train, y_train)  #
+    estimator.fit(x_train, y_train)
 
     # Pronostique para las muestras de entrenamiento y validacion
-    y_trian_pred = ____.____(____)  
-    y_test_pred = ____.____(____)  
+    y_train_pred = estimator.predict(x_train)
+    y_test_pred = estimator.predict(x_test)
 
     # Calcule el error cuadrático medio de las muestras
-    mse_train = ____(  
-        ___,  
-        ___,  
-    )
-    mse_test = ____(  
-        ___,  
-        ___,  
-    )
+    mse_train = mean_squared_error(y_train, y_train_pred)
+    mse_test = mean_squared_error(y_test, y_test_pred)
+
 
     # Retorne el mse de entrenamiento y prueba
     return mse_train, mse_test
